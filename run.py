45bf22b5b8b5cd2cb7c988af79e3cd4ef243a897
alex025x <>
2023-10-12 17:49:36 +0000

    Add play again function, if player wants to restart game after win or loss.

--- run.py
+++ run.py
@@ -4,18 +4,18 @@ import os
 def choose_word(category, difficulty):
     words = {
         "foods": {
-            "easy": ["apple", "banana", "cherry", "date"],
-            "medium": ["papaya", "pineapple", "raspberry"],
-            "hard": ["kiwifruit", "strawberry", "blackberry"]
+            "easy": ["burger", "banana", "sushi", "pizza", "orange"],
+            "medium": ["papaya", "lasagna", "raspberry", "pudding", "kebab"],
+            "hard": ["prune", "strawberry", "wasabi", "nachos", "borscht"]
         },
         "cars": {
-            "easy": ["bmw", "audi", "ford"],
-            "medium": ["toyota", "chevrolet", "honda"],
-            "hard": ["mercedes", "ferrari", "lamborghini"]
+            "easy": ["bmw", "audi", "ford", "mini", "lexus", "subaru"],
+            "medium": ["toyota", "chevrolet", "honda", "dacia", "porsche", "opel"],
+            "hard": ["mercedes", "ferrari", "lamborghini", "delorean", "koenigsegg"]
         },
         "countries": {
             "easy": ["usa", "austria", "turkey", "hungary", "sweden", "russia"],
-            "medium": ["afghanistan", "kazachstan", "turkmenistan", "thailand"],
+            "medium": ["afghanistan", "kazachstan", "turkmenistan", "thailand", "panama"],
             "hard": ["liechtenstein", "kenya", "madagascar", "myanmar", "bangladesh", "nepal"]
         }
     }
@@ -97,6 +97,14 @@ def display_hangman(tries):
     ]
     return stages[tries]
 
+def play_again():
+    while True:
+        play_again_input = input("Would you like to play again? (y/n) ").strip().lower()
+        if play_again_input in ['y', 'n']:
+            return play_again_input == 'y'  
+        else:
+            print("Invalid input. Please enter 'y' or 'n'.")
+
 def play_hangman():
     print("Welcome to Hangman!")
     start_game = input("Press 'y' to start the game: ").strip().lower()
@@ -123,26 +131,26 @@ def play_hangman():
     print(word_completion)
     print("\n")
     
-    while not guessed and tries > 0:
+    while not guessed and tries > 0:  # Main game loop
         guess = input("Please guess a letter: ").lower()
         
-        if len(guess) == 1 and guess.isalpha():
+        if len(guess) == 1 and guess.isalpha(): # Validate input
             if guess in guessed_letters:
-                print("You already guessed", guess)
+                print("You already guessed", guess) # Repeat guess message
             elif guess not in word:
-                print(guessed, "is not in the word.")
+                print(guess, "is not in the word.")
                 tries -= 1
-                guessed_letters.append(guess)
+                guessed_letters.append(guess) # Track guessed letter
                 print("Guessed Letters:", guessed_letters)
                 print("Remaining tries:", tries)
             else:
                 print("Good job,", guess, "is in the word!")
                 guessed_letters.append(guess)
-                word_as_list = list(word_completion)
+                word_as_list = list(word_completion) # Convert to list
                 indices = [i for i, letter in enumerate(word) if letter == guess]
                 for index in indices:
-                    word_as_list[index] = guess
-                word_completion = "".join(word_as_list)
+                    word_as_list[index] = guess # Update guessed letters
+                word_completion = "".join(word_as_list) # Convert back to string
                 if "_" not in word_completion:
                     guessed = True
 
@@ -155,5 +163,9 @@ def play_hangman():
     else:
         print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
 
+    return play_again()
+
 if __name__ == "__main__":
-    play_hangman()
+    while play_hangman():
+        pass
+    print("Thank you for playing, have a nice day!")
