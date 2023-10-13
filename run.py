08b611c5c80f8881777625f34125470c4ae4c230
alex025x <>
2023-10-13 12:11:56 +0000

    Fix validator errors

--- run.py
+++ run.py
@@ -1,25 +1,53 @@
 import random
 import os
 
+
 def choose_word(category, difficulty):
     words = {
         "foods": {
-            "easy": ["burger", "banana", "sushi", "pizza", "orange", "shrimp", "steak", "cake"],
-            "medium": ["papaya", "lasagna", "raspberry", "pudding", "kebab", "paprika", "soup"],
-            "hard": ["prune", "strawberry", "wasabi", "nachos", "borscht", "quinoa", "gnocchi"]
+            "easy": [
+                "burger", "banana", "sushi", "pizza", "orange",
+                "shrimp", "steak", "cake"
+            ],
+            "medium": [
+                "papaya", "lasagna", "raspberry", "pudding",
+                "kebab", "paprika", "soup"
+            ],
+            "hard": [
+                "prune", "strawberry", "wasabi", "nachos",
+                "borscht", "quinoa", "gnocchi"
+            ]
         },
         "cars": {
-            "easy": ["bmw", "audi", "ford", "mini", "lexus", "subaru", "mazda", "renault", "fiat", "hyundai", "kia"],
-            "medium": ["toyota", "chevrolet", "honda", "dacia", "porsche", "opel", "pagani", "bentley", "volkswagen"],
-            "hard": ["mercedes", "ferrari", "lamborghini", "delorean", "koenigsegg", "detomaso", "holden", "rimac"]
+            "easy": [
+                "bmw", "audi", "ford", "mini", "lexus", "subaru",
+                "mazda", "renault", "fiat", "hyundai", "kia"
+            ],
+            "medium": [
+                "toyota", "chevrolet", "honda", "dacia",
+                "porsche", "opel", "pagani", "bentley", "volkswagen"
+            ],
+            "hard": [
+                "mercedes", "ferrari", "lamborghini", "delorean",
+                "koenigsegg", "detomaso", "holden", "rimac"
+            ]
         },
         "countries": {
-            "easy": ["usa", "austria", "turkey", "hungary", "sweden", "russia", "italy", "mexico", "belgium"],
-            "medium": ["afghanistan", "kazachstan", "turkmenistan", "thailand", "panama", "switzerland", "netherlands"],
-            "hard": ["liechtenstein", "kenya", "madagascar", "myanmar", "bangladesh", "nepal", "djibouti", "kyrgyzstan"]
+            "easy": [
+                "usa", "austria", "turkey", "hungary", "sweden",
+                "russia", "italy", "mexico", "belgium"
+            ],
+            "medium": [
+                "afghanistan", "kazachstan", "turkmenistan",
+                "thailand", "panama", "switzerland", "netherlands"
+            ],
+            "hard": [
+                "liechtenstein", "kenya", "madagascar", "myanmar",
+                "bangladesh", "nepal", "djibouti", "kyrgyzstan"
+            ]
         }
     }
-    
+
     return random.choice(words[category][difficulty])
 
 
@@ -41,7 +69,7 @@ def display_hangman(tries):
                    |      O
                    |     \\|/
                    |      |
-                   |     / 
+                   |     /
                    -
                 """,
                 # head, torso, and both arms
@@ -51,7 +79,7 @@ def display_hangman(tries):
                    |      O
                    |     \\|/
                    |      |
-                   |      
+                   |
                    -
                 """,
                 # head, torso, and one arm
@@ -61,7 +89,7 @@ def display_hangman(tries):
                    |      O
                    |     \\|
                    |      |
-                   |     
+                   |
                    -
                 """,
                 # head and torso
@@ -71,7 +99,7 @@ def display_hangman(tries):
                    |      O
                    |      |
                    |      |
-                   |     
+                   |
                    -
                 """,
                 # head
@@ -79,28 +107,28 @@ def display_hangman(tries):
                    --------
                    |      |
                    |      O
-                   |    
-                   |      
-                   |     
+                   |
+                   |
+                   |
                    -
                 """,
                 # initial empty state
                 """
                    --------
                    |      |
-                   |      
-                   |    
-                   |      
-                   |     
+                   |
+                   |
+                   |
+                   |
                    -
                 """
-                
+
     ]
     return stages[tries]
 
+
 def print_logo_and_explanation():
     logo = """
- 
     ██╗░░██╗░█████╗░███╗░░██╗░██████╗░███╗░░░███╗░█████╗░███╗░░██╗
     ██║░░██║██╔══██╗████╗░██║██╔════╝░████╗░████║██╔══██╗████╗░██║
     ███████║███████║██╔██╗██║██║░░██╗░██╔████╔██║███████║██╔██╗██║
@@ -120,60 +148,67 @@ def print_logo_and_explanation():
     )
     print(explanation)
 
+
 def play_again():
     while True:
-        play_again_input = input("Would you like to play again? (y/n) ").strip().lower()
+        play_again_input = input(
+            "Would you like to play again? (y/n) ").strip().lower()
         if play_again_input in ['y', 'n']:
-            return play_again_input == 'y'  
+            return play_again_input == 'y'
         else:
             print("Invalid input. Please enter 'y' or 'n'.")
+
 
 def play_hangman():
     print_logo_and_explanation()
     start_game = input("Press 'y' to start the game: ").strip().lower()
-    
+
     if start_game != 'y':
         return
 
-    category = ""
-    while category not in ["foods", "cars", "countries"]:
-        category = input("Which category of words would you like to guess (foods/cars/countries)? ").strip().lower()
-    
+    category = input(
+        "Which category of words would you like to guess "
+        "(foods/cars/countries)? "
+    ).strip().lower()
+
     difficulty = ""
     while difficulty not in ["easy", "medium", "hard"]:
-        difficulty = input("Choose word difficulty (easy, medium, hard): ").strip().lower()
-    
+        difficulty = input(
+            "Choose word difficulty (easy, medium, hard): ").strip().lower()
+
     word = choose_word(category, difficulty)
-    word_completion = "_" * len(word)  
-    guessed = False  
+    word_completion = "_" * len(word)
+    guessed = False
     guessed_letters = []
     tries = 6
-    
+
     print("Let's play Hangman!")
     print(display_hangman(tries))
     print(word_completion)
     print("\n")
-    
+
     while not guessed and tries > 0:  # Main game loop
         guess = input("Please guess a letter: ").lower()
-        
-        if len(guess) == 1 and guess.isalpha(): # Validate input
+
+        if len(guess) == 1 and guess.isalpha():  # Validate input
             if guess in guessed_letters:
-                print("You already guessed", guess) # Repeat guess message
+                print("You already guessed", guess)  # Repeat guess message
             elif guess not in word:
                 print(guess, "is not in the word.")
                 tries -= 1
-                guessed_letters.append(guess) # Track guessed letter
+                guessed_letters.append(guess)  # Track guessed letter
                 print("Guessed Letters:", guessed_letters)
                 print("Remaining tries:", tries)
             else:
                 print("Good job,", guess, "is in the word!")
                 guessed_letters.append(guess)
-                word_as_list = list(word_completion) # Convert to list
-                indices = [i for i, letter in enumerate(word) if letter == guess]
+                word_as_list = list(word_completion)  # Convert to list
+                indices = [i for i, letter in enumerate(
+                    word) if letter == guess]
                 for index in indices:
-                    word_as_list[index] = guess # Update guessed letters
-                word_completion = "".join(word_as_list) # Convert back to string
+                    word_as_list[index] = guess  # Update guessed letters
+                # Convert back to string
+                word_completion = "".join(word_as_list)
                 if "_" not in word_completion:
                     guessed = True
         elif len(guess) > 1:
@@ -181,22 +216,22 @@ def play_hangman():
 
         else:
             print("Error: Only letters allowed")
-            continue # Skip the rest of the loop if the input is invalid
+            continue  # Skip the rest of the loop if the input is invalid
 
         print(display_hangman(tries))
         print(word_completion)
         print("\n")
-        
+
     if guessed:
         print("Congrats, you guessed the word! You win ( ͡ᵔ ͜ʖ ͡ᵔ)")
     else:
-        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time! ( ͡ᵔ ︵ ͡ᵔ)")
+        print("Sorry, you ran out of tries. The word was " +
+              word + ". Maybe next time! ( ͡ᵔ ︵ ͡ᵔ)")
 
     return play_again()
 
+
 if __name__ == "__main__":
-    while play_hangman(): # Executes hangman game in a continuous loop.
-        pass # If user chooses no prints message"
+    while play_hangman():  # Executes hangman game in a continuous loop.
+        pass  # If user chooses no, print message
     print("Thank you for playing, have a nice day! ┏( ͡ᵔ _⦣ ͡ᵔ)┛ ")
-
-
